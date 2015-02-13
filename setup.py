a53e0f8518c5d6428efa765ba94cd3ea54f1c30a
Andrew Grigorev <andrew@ei-grad.ru>
2015-02-13 16:32:31 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='yatank-online',
+      version='0.0',
+      description='Yandex.Tank OnlineReport plugin',
+      author='Alexey Lavrenuke',
+      author_email='direvius@gmail.com',
+      url='https://github.com/yandex-load/yatank-online/',
+      packages=['yandextank.plugins.OnlineReport'],
+     )
