4e77bd0fc6da89930c8a5918cb499092e0c15d31
Alexey Lavrenuke <direvius@gmail.com>
2016-03-23 14:34:55 +0300

    version

--- setup.py
+++ setup.py
@@ -2,26 +2,24 @@
 
 from setuptools import setup
 
-setup(
-    name='yatank-online',
-    version='0.0.8',
-    description='Yandex.Tank OnlineReport plugin',
-    author='Alexey Lavrenuke',
-    author_email='direvius@gmail.com',
-    url='https://github.com/yandex-load/yatank-online/',
-    packages=['yatank_OnlineReport'],
-    package_data={'yatank_OnlineReport': [
-        'templates/*',
-        'static/favicon.ico',
-        'static/css/*',
-        'static/js/*.js',
-        'static/js/*.coffee',
-        'static/js/vendor/*.js',
-        'static/fonts/*',
-    ]},
-    install_requires=[
-        'tornado',
-        'tornadio2',
-        'pyjade>=4.0.0',
-    ],
-)
+setup(name='yatank-online',
+      version='0.0.9',
+      description='Yandex.Tank OnlineReport plugin',
+      author='Alexey Lavrenuke',
+      author_email='direvius@gmail.com',
+      url='https://github.com/yandex-load/yatank-online/',
+      packages=['yatank_OnlineReport'],
+      package_data={'yatank_OnlineReport': [
+          'templates/*',
+          'static/favicon.ico',
+          'static/css/*',
+          'static/js/*.js',
+          'static/js/*.coffee',
+          'static/js/vendor/*.js',
+          'static/fonts/*',
+      ]},
+      install_requires=[
+          'tornado',
+          'tornadio2',
+          'pyjade>=4.0.0',
+      ], )
